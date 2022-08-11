9fd3f9af30fcdb05d043e1c235a89b628e4c10c2
Adibah23 <adibah.nordin23@gmail.com>
2022-08-11 17:55:55 +0800

    update home & form UI

--- app.py
+++ app.py
@@ -1,18 +1,41 @@
 from flask import Flask, render_template
 
 
-app = Flask(__name__, static_folder="static",template_folder="templates")
+app = Flask(__name__, static_folder="static", template_folder="templates")
 app.config['SECRET_KEY'] = 'any secret key'
 
 # Meta
 META_IMAGE_LINK = ""
 META_DESCRIPTION = ""
 
+
 @app.route("/")
 def index():
-    return render_template("index.html", title='RoboTakaful',
+    return render_template("home.html", title='RoboTakaful',
                            meta_description=META_DESCRIPTION,
                            meta_image=META_IMAGE_LINK)
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html", title='RoboTakaful | Login',
+                           meta_description=META_DESCRIPTION,
+                           meta_image=META_IMAGE_LINK)
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html", title='RoboTakaful | SignUp',
+                           meta_description=META_DESCRIPTION,
+                           meta_image=META_IMAGE_LINK)
+
+
+@app.route("/compare")
+def compare():
+    return render_template("wizard.html", title='RoboTakaful | Compare Tool',
+                           meta_description=META_DESCRIPTION,
+                           meta_image=META_IMAGE_LINK)
+
 
 if __name__ == '__main__':
     # HOST = "0.0.0.0"
