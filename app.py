1fb927960f05cd32bd746be9748a9b38f54acb29
adibahnordin98 <nuradibahnordin2604@gmail.com>
2022-09-02 11:00:50 +0800

    add etiqa

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, json,jsonify
 
 
 app = Flask(__name__, static_folder="static", template_folder="templates")
@@ -45,74 +45,265 @@ def employee_info():
 
 @app.route("/quote")
 def quote():
-    empNo = request.args.get('empNo')
+    employeeAgeList = request.args.get('empAge')
+    employeeNoList = request.args.get('empNo')
     empCoverage = request.args.get('empCoverage')
-    empList =[]
+    totalContGreatEastern = 0
+    totalContEtiqa = 0
+    empAge = []
+    empNo = []
     totalNoEmp = 0
-    
-    for item in empNo.split(','):
+
+    for item in employeeNoList.split(','):
         if(item.isnumeric()):
-            empList.append(int(item))
+            empNo.append(int(item))
 
-    if int(empCoverage) == 10000:
-        totalCont = (empList[0] * 17.70 + empList[1] * 17.70 + empList[2] * 17.70 + empList[3] * 17.70 
-        + empList[4] * 19.90 + empList[5] * 31.90 + empList[6] * 55.40 + empList[7] * 91.70
-        + empList[8] * 151.80 + empList[9] * 262.30 + empList[10] * 435.90)
-
-    elif int(empCoverage) == 30000:
-        totalCont = (empList[0] * 36.30 + empList[1] * 36.30 + empList[2] * 36.30 + empList[3] * 36.30
-        + empList[4] * 40.90 + empList[5] * 65.50 + empList[6] * 113.80 + empList[7] * 188.50
-        + empList[8] * 312.00 + empList[9] * 538.70 + empList[10] * 895.70)
-
-    elif int(empCoverage) == 50000:
-        totalCont = (empList[0] * 54.90 + empList[1] * 54.90 + empList[2] * 54.90 + empList[3] * 54.90
-        + empList[4] * 61.90 + empList[5] * 99.10 + empList[6] * 172.20 + empList[7] * 285.30
-        + empList[8] * 472.20 + empList[9] * 815.10 + empList[10] * 1355.50)
-
-    elif int(empCoverage) == 100000:
-        totalCont = (empList[0] * 101.40 + empList[1] * 101.40 + empList[2] * 101.40 + empList[3] * 101.40
-        + empList[4] * 114.40 + empList[5] * 183.10 + empList[6] * 318.20 + empList[7] * 527.30
-        + empList[8] * 872.70 + empList[9] * 1506.10 + empList[10] * 2505.00)
-
-    elif int(empCoverage) == 200000:
-        totalCont = (empList[0] * 192.40 + empList[1] * 192.40 + empList[2] * 192.40 + empList[3] * 192.40
-        + empList[4] * 217.40 + empList[5] * 348.10 + empList[6] * 606.20 + empList[7] * 1003.30
-        + empList[8] * 1661.70 + empList[9] * 2869.10 + empList[10] * 4772.00)
-
-    elif int(empCoverage) == 300000:
-        totalCont = (empList[0] * 283.40 + empList[1] * 283.40 + empList[2] * 283.40 + empList[3] * 283.40
-        + empList[4] * 320.40 + empList[5] * 513.10 + empList[6] * 849.20 + empList[7] * 1479.30
-        + empList[8] * 2450.70 + empList[9] * 4232.10 + empList[10] * 7039.00)
-
-    elif int(empCoverage) == 400000:
-        totalCont = (empList[0] * 374.40 + empList[1] * 374.40 + empList[2] * 374.40 + empList[3] * 374.40
-        + empList[4] * 423.40 + empList[5] * 678.10 + empList[6] * 1182.20 + empList[7] * 1955.30
-        + empList[8] * 3239.70 + empList[9] * 5595.10 + empList[10] * 9306.00)
+    for item in employeeAgeList.split(','):
+        if(item.isnumeric()):
+            empAge.append(int(item))
             
-    elif int(empCoverage) == 500000:
-        totalCont = (empList[0] * 465.40 + empList[1] * 465.40 + empList[2] * 465.40 + empList[3] * 465.40
-        + empList[4] * 526.40 + empList[5] * 843.10 + empList[6] * 1470.20 + empList[7] * 2431.30
-        + empList[8] * 4028.70 + empList[9] * 6958.10 + empList[10] * 11573.00)
+    for idx, empAge in enumerate(empAge):
 
-    serviceTax = 0.06 * totalCont
-    stampDuty = 10.00
-    grandTotalCont = totalCont + serviceTax + stampDuty
+        totalNoEmp += int(empNo[idx])
 
-    for item in empList:
-        totalNoEmp = totalNoEmp + item
+        # Great Eastern Takaful
+        if int(empCoverage) == 10000:
+            if int(empAge >= 17) and int(empAge <= 35):
+                totalContGreatEastern = totalContGreatEastern + 17.70 * empNo[idx]
+            elif int(empAge >= 36) and int(empAge <= 40):
+                totalContGreatEastern = totalContGreatEastern + 19.90 * empNo[idx]
+            elif int(empAge >= 41) and int(empAge <= 45):
+                totalContGreatEastern = totalContGreatEastern + 31.90 * empNo[idx]
+            elif int(empAge >= 46) and int(empAge <= 50):
+                totalContGreatEastern = totalContGreatEastern + 55.40 * empNo[idx]
+            elif int(empAge >= 51) and int(empAge <= 55):
+                totalContGreatEastern = totalContGreatEastern + 91.70 * empNo[idx]
+            elif int(empAge >= 56) and int(empAge <= 60):
+                totalContGreatEastern = totalContGreatEastern + 151.80 * empNo[idx]
+            elif int(empAge >= 61) and int(empAge <= 65):
+                totalContGreatEastern = totalContGreatEastern + 262.30 * empNo[idx]
+            elif int(empAge >= 66) and int(empAge <= 70):
+                totalContGreatEastern = totalContGreatEastern + 435.90 * empNo[idx]
 
+        elif int(empCoverage) == 30000:
+            if int(empAge >= 17) and int(empAge <= 35):
+                totalContGreatEastern = totalContGreatEastern + 36.30 * empNo[idx]
+            elif int(empAge >= 36) and int(empAge <= 40):
+                totalContGreatEastern = totalContGreatEastern + 40.90 * empNo[idx]
+            elif int(empAge >= 41) and int(empAge <= 45):
+                totalContGreatEastern = totalContGreatEastern + 65.50 * empNo[idx]
+            elif int(empAge >= 46) and int(empAge <= 50):
+                totalContGreatEastern = totalContGreatEastern + 113.80 * empNo[idx]
+            elif int(empAge >= 51) and int(empAge <= 55):
+                totalContGreatEastern = totalContGreatEastern + 188.50 * empNo[idx]
+            elif int(empAge >= 56) and int(empAge <= 60):
+                totalContGreatEastern = totalContGreatEastern + 312.00 * empNo[idx]
+            elif int(empAge >= 61) and int(empAge <= 65):
+                totalContGreatEastern = totalContGreatEastern + 538.70 * empNo[idx]
+            elif int(empAge >= 66) and int(empAge <= 70):
+                totalContGreatEastern = totalContGreatEastern + 895.70 * empNo[idx]
+
+        elif int(empCoverage) == 50000:
+            if int(empAge >= 17) and int(empAge <= 35):
+                totalContGreatEastern = totalContGreatEastern + 54.90 * empNo[idx]
+            elif int(empAge >= 36) and int(empAge <= 40):
+                totalContGreatEastern = totalContGreatEastern + 61.90 * empNo[idx]
+            elif int(empAge >= 41) and int(empAge <= 45):
+                totalContGreatEastern = totalContGreatEastern + 99.10 * empNo[idx]
+            elif int(empAge >= 46) and int(empAge <= 50):
+                totalContGreatEastern = totalContGreatEastern + 172.20 * empNo[idx]
+            elif int(empAge >= 51) and int(empAge <= 55):
+                totalContGreatEastern = totalContGreatEastern + 285.30 * empNo[idx]
+            elif int(empAge >= 56) and int(empAge <= 60):
+                totalContGreatEastern = totalContGreatEastern + 472.20 * empNo[idx]
+            elif int(empAge >= 61) and int(empAge <= 65):
+                totalContGreatEastern = totalContGreatEastern + 815.10 * empNo[idx]
+            elif int(empAge >= 66) and int(empAge <= 70):
+                totalContGreatEastern = totalContGreatEastern + 11355.50 * empNo[idx]
+
+        elif int(empCoverage) == 100000:
+            if int(empAge >= 17) and int(empAge <= 35):
+                totalContGreatEastern = totalContGreatEastern + 101.40 * empNo[idx]
+            elif int(empAge >= 36) and int(empAge <= 40):
+                totalContGreatEastern = totalContGreatEastern + 114.40 * empNo[idx]
+            elif int(empAge >= 41) and int(empAge <= 45):
+                totalContGreatEastern = totalContGreatEastern + 183.10 * empNo[idx]
+            elif int(empAge >= 46) and int(empAge <= 50):
+                totalContGreatEastern = totalContGreatEastern + 318.20 * empNo[idx]
+            elif int(empAge >= 51) and int(empAge <= 55):
+                totalContGreatEastern = totalContGreatEastern + 527.30 * empNo[idx]
+            elif int(empAge >= 56) and int(empAge <= 60):
+                totalContGreatEastern = totalContGreatEastern + 872.70 * empNo[idx]
+            elif int(empAge >= 61) and int(empAge <= 65):
+                totalContGreatEastern = totalContGreatEastern + 1506.10 * empNo[idx]
+            elif int(empAge >= 66) and int(empAge <= 70):
+                totalContGreatEastern = totalContGreatEastern + 2505.00 * empNo[idx]
+
+        elif int(empCoverage) == 200000:
+            if int(empAge >= 17) and int(empAge <= 35):
+                totalContGreatEastern = totalContGreatEastern + 192.40 * empNo[idx]
+            elif int(empAge >= 36) and int(empAge <= 40):
+                totalContGreatEastern = totalContGreatEastern + 217.40 * empNo[idx]
+            elif int(empAge >= 41) and int(empAge <= 45):
+                totalContGreatEastern = totalContGreatEastern + 348.10 * empNo[idx]
+            elif int(empAge >= 46) and int(empAge <= 50):
+                totalContGreatEastern = totalContGreatEastern + 606.20 * empNo[idx]
+            elif int(empAge >= 51) and int(empAge <= 55):
+                totalContGreatEastern = totalContGreatEastern + 1003.30 * empNo[idx]
+            elif int(empAge >= 56) and int(empAge <= 60):
+                totalContGreatEastern = totalContGreatEastern + 1661.70 * empNo[idx]
+            elif int(empAge >= 61) and int(empAge <= 65):
+                totalContGreatEastern = totalContGreatEastern + 2869.10 * empNo[idx]
+            elif int(empAge >= 66) and int(empAge <= 70):
+                totalContGreatEastern = totalContGreatEastern + 4772.00 * empNo[idx]
+
+        elif int(empCoverage) == 300000:
+            if int(empAge >= 17) and int(empAge <= 35):
+                totalContGreatEastern = totalContGreatEastern + 283.40 * empNo[idx]
+            elif int(empAge >= 36) and int(empAge <= 40):
+                totalContGreatEastern = totalContGreatEastern + 320.40 * empNo[idx]
+            elif int(empAge >= 41) and int(empAge <= 45):
+                totalContGreatEastern = totalContGreatEastern + 513.10 * empNo[idx]
+            elif int(empAge >= 46) and int(empAge <= 50):
+                totalContGreatEastern = totalContGreatEastern + 894.20 * empNo[idx]
+            elif int(empAge >= 51) and int(empAge <= 55):
+                totalContGreatEastern = totalContGreatEastern + 1479.30 * empNo[idx]
+            elif int(empAge >= 56) and int(empAge <= 60):
+                totalContGreatEastern = totalContGreatEastern + 2450.70 * empNo[idx]
+            elif int(empAge >= 61) and int(empAge <= 65):
+                totalContGreatEastern = totalContGreatEastern + 4232.10 * empNo[idx]
+            elif int(empAge >= 66) and int(empAge <= 70):
+                totalContGreatEastern = totalContGreatEastern + 7039.00 * empNo[idx]
+
+        elif int(empCoverage) == 400000:
+            if int(empAge >= 17) and int(empAge <= 35):
+                totalContGreatEastern = totalContGreatEastern + 374.40 * empNo[idx]
+            elif int(empAge >= 36) and int(empAge <= 40):
+                totalContGreatEastern = totalContGreatEastern + 423.40 * empNo[idx]
+            elif int(empAge >= 41) and int(empAge <= 45):
+                totalContGreatEastern = totalContGreatEastern + 678.10 * empNo[idx]
+            elif int(empAge >= 46) and int(empAge <= 50):
+                totalContGreatEastern = totalContGreatEastern + 1182.20 * empNo[idx]
+            elif int(empAge >= 51) and int(empAge <= 55):
+                totalContGreatEastern = totalContGreatEastern + 1955.30 * empNo[idx]
+            elif int(empAge >= 56) and int(empAge <= 60):
+                totalContGreatEastern = totalContGreatEastern + 3239.70 * empNo[idx]
+            elif int(empAge >= 61) and int(empAge <= 65):
+                totalContGreatEastern = totalContGreatEastern + 5595.10 * empNo[idx]
+            elif int(empAge >= 66) and int(empAge <= 70):
+                totalContGreatEastern = totalContGreatEastern + 9306.00 * empNo[idx]
+
+        elif int(empCoverage) == 500000:
+            if int(empAge >= 17) and int(empAge <= 35):
+                totalContGreatEastern = totalContGreatEastern + 465.40 * empNo[idx]
+            elif int(empAge >= 36) and int(empAge <= 40):
+                totalContGreatEastern = totalContGreatEastern + 526.40 * empNo[idx]
+            elif int(empAge >= 41) and int(empAge <= 45):
+                totalContGreatEastern = totalContGreatEastern + 843.10 * empNo[idx]
+            elif int(empAge >= 46) and int(empAge <= 50):
+                totalContGreatEastern = totalContGreatEastern + 14720.20 * empNo[idx]
+            elif int(empAge >= 51) and int(empAge <= 55):
+                totalContGreatEastern = totalContGreatEastern + 2431.30 * empNo[idx]
+            elif int(empAge >= 56) and int(empAge <= 60):
+                totalContGreatEastern = totalContGreatEastern + 4028.70 * empNo[idx]
+            elif int(empAge >= 61) and int(empAge <= 65):
+                totalContGreatEastern = totalContGreatEastern + 6958.10 * empNo[idx]
+            elif int(empAge >= 66) and int(empAge <= 70):
+                totalContGreatEastern = totalContGreatEastern + 11573.00 * empNo[idx]
+
+        # Great Eastern Takaful
+        if int(empCoverage) == 20000:
+            if int(empAge >= 16) and int(empAge <= 69):
+                totalContEtiqa = totalContEtiqa + 28.00 * empNo[idx]
+            elif int(empAge >= 70) and int(empAge <= 75):
+                totalContEtiqa = totalContEtiqa + 1845.00 * empNo[idx]
+
+        elif int(empCoverage) == 50000:
+            if int(empAge >= 16) and int(empAge <= 69):
+                totalContEtiqa = totalContEtiqa + 70.00 * empNo[idx]
+            elif int(empAge >= 70) and int(empAge <= 75):
+                totalContEtiqa = totalContEtiqa + 4613.00 * empNo[idx]
+
+        elif int(empCoverage) == 100000:
+            if int(empAge >= 16) and int(empAge <= 69):
+                totalContEtiqa = totalContEtiqa + 140.00 * empNo[idx]
+            elif int(empAge >= 70) and int(empAge <= 75):
+                totalContEtiqa = totalContEtiqa + 9226.00 * empNo[idx]
+
+        elif int(empCoverage) == 150000:
+            if int(empAge >= 16) and int(empAge <= 69):
+                totalContEtiqa = totalContEtiqa + 210.00 * empNo[idx]
+            elif int(empAge >= 70) and int(empAge <= 75):
+                totalContEtiqa = totalContEtiqa + 13839.00 * empNo[idx]
+
+        elif int(empCoverage) == 200000:
+            if int(empAge >= 16) and int(empAge <= 69):
+                totalContEtiqa = totalContEtiqa + 280.00 * empNo[idx]
+            elif int(empAge >= 70) and int(empAge <= 75):
+                totalContEtiqa = totalContEtiqa + 18452.00 * empNo[idx]
+
+        elif int(empCoverage) == 250000:
+            if int(empAge >= 16) and int(empAge <= 69):
+                totalContEtiqa = totalContEtiqa + 350.00 * empNo[idx]
+            elif int(empAge >= 70) and int(empAge <= 75):
+                totalContEtiqa = totalContEtiqa + 23065.00 * empNo[idx]
+
+        elif int(empCoverage) == 300000:
+            if int(empAge >= 16) and int(empAge <= 69):
+                totalContEtiqa = totalContEtiqa + 420.00 * empNo[idx]
+            elif int(empAge >= 70) and int(empAge <= 75):
+                totalContEtiqa = totalContEtiqa + 27678.00 * empNo[idx]
+        
     totalSumAssured = "{:,}".format(totalNoEmp * int(empCoverage))
+
+    serviceTax, stampDuty, grandTotalCont = roboCalculate(totalContGreatEastern)
+
+    if totalContGreatEastern != 0:
+        greatEasternDict = {
+            "takafulName": "Great Eastern",
+            "totalCont" : '{:.2f}'.format(round(totalContGreatEastern, 2)),
+            "serviceTax" : '{:.2f}'.format(round(serviceTax, 2)),
+            "stampDuty" : '{:.2f}'.format(round(stampDuty, 2)),
+            "grandTotalCont" : '{:.2f}'.format(round(grandTotalCont, 2))
+        }
+    else:
+        greatEasternDict = {
+                "takafulName": "None",
+        }
+
+    serviceTax, stampDuty, grandTotalCont = roboCalculate(totalContEtiqa)
+
+    if totalContEtiqa != 0:
+        etiqaDict = {
+            "takafulName": "Great Eastern",
+            "totalCont" : '{:.2f}'.format(round(totalContEtiqa, 2)),
+            "serviceTax" : '{:.2f}'.format(round(serviceTax, 2)),
+            "stampDuty" : '{:.2f}'.format(round(stampDuty, 2)),
+            "grandTotalCont" : '{:.2f}'.format(round(grandTotalCont, 2))
+        }
+    else:
+        etiqaDict = {
+                "takafulName": "None",
+        }
+        
 
     return render_template("quote.html", title='RoboTakaful | Quotation',
                            meta_description=META_DESCRIPTION,
                            meta_image=META_IMAGE_LINK,
-                           totalCont='{:.2f}'.format(round(totalCont, 2)),
-                           serviceTax='{:.2f}'.format(round(serviceTax, 2)),
-                           stampDuty='{:.2f}'.format(round(stampDuty, 2)),
-                           grandTotalCont='{:.2f}'.format(round(grandTotalCont, 2)),
+                           greatEasternDict = greatEasternDict,
+                           etiqaDict = etiqaDict,
                            totalSumAssured=totalSumAssured,
-                           totalNoEmp=totalNoEmp)
+                           totalNoEmp=totalNoEmp,
+                           empCoverage="{:,}".format(int(empCoverage)))
 
+############################################################### FUNCTIONS ############################################################### 
+
+def roboCalculate(totalCont):
+    serviceTax = 0.06 * totalCont
+    stampDuty = 10.00
+    grandTotalCont = totalCont + serviceTax + stampDuty
+
+    return serviceTax, stampDuty, grandTotalCont
 
 if __name__ == '__main__':
     # HOST = "0.0.0.0"
--- static/js/employee.js
+++ static/js/employee.js
@@ -1,59 +1,125 @@
+var employeeAgeList
+var employeeNoList
 
-var totalNoEmp
-var empNo
 
 function employeeCalc(){
+    roboCalc()
+}
 
-    calclEmployeeNo()
+function roboCalc(){
+    employeeNoList = []
+    employeeAgeList = []
+    var totalNoEmp = 0
+    var table = document.getElementById("employeeTable");
+    for (var i = 1, row; row = table.rows[i]; i++) {
+        var tempStr = "no" + String(i)
+        var elements = document.getElementById(tempStr)
 
-    if (typeof totalNoEmp === 'number' && !Number.isNaN(totalNoEmp)) {
+        var tempStr2 = "age" + String(i)
+        var elements2 = document.getElementById(tempStr2)
+
+        if(elements == null)
+            continue
+        else
+            var no = elements.value
+
+        if(elements2 == null)
+            continue
+        else
+            var age = elements2.value
+
+        let isnum = /^\d+$/.test(no);
+        let isnumage = /^\d+$/.test(age);
+
+        if(isnum && isnumage){
+            totalNoEmp += parseInt(no)
+            employeeNoList[i-1] = parseInt(elements.value)
+            employeeAgeList[i-1] = parseInt(elements2.value)
+        }
+        else{
+            totalNoEmp = "Not Number"
+        }
+    }
+
+    totalSumAssured = totalNoEmp * document.getElementById("employeeCoverage").value
+
+    if (typeof totalNoEmp === 'number' && !Number.isNaN(totalNoEmp) && totalNoEmp != null) {
         document.getElementById("employeeTotalNo").value = totalNoEmp
-        calcTotalSumAssured()
+        document.getElementById("employeeTotalSumAssured").value = totalSumAssured.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
     }
     else{
         document.getElementById("employeeTotalNo").value = "Invalid. Please recheck your input."
+        document.getElementById("employeeTotalSumAssured").value = "Invalid. Please recheck your input."
     }
 
-    return empNo
 }
-
-function employeeCoverage(){
-    calclEmployeeNo()
-    calcTotalSumAssured()
-}
-
-function calclEmployeeNo(){
-    empNo = []
-    totalNoEmp = 0
-    
-    for(let i=0; i<11; i++){
-        let tempCount = i+1
-        let tempStr = "no"+tempCount
-        empNo[i] = parseInt(document.getElementById(tempStr).value)
-        totalNoEmp += empNo[i]
-    }
-}
-
-function calcTotalSumAssured(){
-    totalSumAssured = totalNoEmp * document.getElementById("employeeCoverage").value
-    document.getElementById("employeeTotalSumAssured").value = totalSumAssured.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-}
-
 document.getElementById('quote').addEventListener('click', ()=>{
     
-    var empNo = employeeCalc()
+    roboCalc()
     var empCoverage = document.getElementById("employeeCoverage").value
-    var totalEmp = document.getElementById("employeeTotalNo").value
-    
 
     if (document.getElementById("employeeTotalNo").value != "Invalid. Please recheck your input."){
-        if (totalEmp <5)
-            alert("Minimum number of employees allowed is (5)")
-        else if(totalEmp >150)
-            alert("Maximum number of employees allowed is (150)")
-        else
-            window.location.href = "/quote?empNo=" + empNo + "&empCoverage=" + empCoverage
+        // if (totalEmp <5)
+        //     alert("Minimum number of employees allowed is (5)")
+        // else if(totalEmp >150)
+        //     alert("Maximum number of employees allowed is (150)")
+        // else
+            window.location.href = "/quote?empAge=" + employeeAgeList + "&empNo=" + employeeNoList + "&empCoverage=" + empCoverage
     }
     else
         alert("Invalid. Please recheck your input before submit.")
 });
+
+$(document).ready(function () {
+  
+    // Denotes total number of rows
+    var rowIdx = 1;
+
+    // jQuery button click event to add a row
+    $('.add-row').on('click', function () {    
+      // Adding a row inside the tbody.
+      $('tbody').append(`<tr id="R${++rowIdx}">
+           <td><input type='text' class='form-control text-center' id='age${rowIdx}' value='0' pattern='[0-9]+'' onchange='employeeCalc()' required></td>
+           <td><input type='text' class='form-control text-center' id='no${rowIdx}' value='0' pattern='[0-9]+' onchange='employeeCalc()' required></td>
+           <td><a class='remove'><i class='mdi mdi-minus-circle-outline align-middle minus-icon'></i></a></td>
+           </tr>`);
+
+           roboCalc()
+    });
+
+    // jQuery button click event to remove a row.
+    $('tbody').on('click', '.remove', function () {
+
+      // Getting all the rows next to the row
+      // containing the clicked button
+      var child = $(this).closest('tr').nextAll();
+
+      // Iterating across all the rows 
+      // obtained to change the index
+      child.each(function () {
+
+        // Getting <tr> id.
+        var id = $(this).attr('id');
+
+        // Getting the <p> inside the .row-index class.
+        var idx = $(this).children('.row-index').children('p');
+
+        // Gets the row number from <tr> id.
+        var dig = parseInt(id.substring(1));
+
+        // Modifying row index.
+        idx.html(`Row ${dig - 1}`);
+
+        // Modifying row id.
+        $(this).attr('id', `R${dig - 1}`);
+      });
+
+      // Removing the current row.
+      $(this).closest('tr').remove();
+
+      // Decreasing total number of rows by 1.
+      rowIdx--;
+
+      roboCalc()
+    });
+  });
